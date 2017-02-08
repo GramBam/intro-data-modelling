00d1e6208674b5fc6b974c4f3a255cc1943b294e
Graham Moss <graham.moss@hotmail.com>
2017-02-07 20:39:07 -0500

    completed first 2 exercises

---
+++ recipe_ingredients_app/db/migrate/20170208011803_recipes_ingredients.rb
@@ -0,0 +1,10 @@
+class RecipesIngredients < ActiveRecord::Migration[5.0]
+  def change
+    create_table :recipes_ingredients do |t|
+      t.integer :recipe_id
+      t.integer :ingredient_id
+
+      t.timestamps
+    end
+  end
+end
